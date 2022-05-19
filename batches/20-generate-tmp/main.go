fb1610582214539e9cf125f933f0466e0b49a7b5
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-05-19 09:57:03 +0900

    Update main.go

--- batches/20-generate-tmp/main.go
+++ batches/20-generate-tmp/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 // テキストファイルを作成します。
@@ -17,9 +18,15 @@ func createNewFile(path string, content string) {
 	file.WriteString(content)
 }
 
+func GetCurrentTimestamp() string {
+
+	now := time.Now()
+	return now.String()[0:23]
+}
+
 func main() {
 
-	createNewFile("test.tmp", "Hello, World!")
+	createNewFile("test.tmp", GetCurrentTimestamp())
 
 	fmt.Println("Ok.")
 }
